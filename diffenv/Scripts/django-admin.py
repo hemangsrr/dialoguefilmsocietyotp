73f30dbe27755313b8e6daa294b79c7281fb0f87
Hemang Mohan <hemangshornur@gmail.com>
2022-01-14 09:43:44 +0530

    Initial django project created

---
+++ diffenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\programming\git\hemang srr\dialoguefilmsocietyotp\diffenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
