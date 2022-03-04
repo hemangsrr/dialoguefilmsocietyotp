90233e29d372c0a11bed33e9d188a6f1f348c204
Hemang Mohan <hemangshornur@gmail.com>
2022-03-04 11:15:23 +0530

    Allowed Host + Custom DOmain

--- dialoguewebsite/settings.py
+++ dialoguewebsite/settings.py
@@ -44,7 +44,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['.herokuapp.com'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['dialogueotp.herokuapp.com','.dialoguefilmsocietyotp.in'])
 
 
 # Application definition
